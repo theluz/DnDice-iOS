1bef382db3937c71b16e787851915100b893e297
theluz <ti.rodrigoluz@gmail.com>
2019-07-15 15:37:20 -0300

    Changed the images names (d0 to d5) so no I wouldn't have to change the Int.random part when not using the array. Added an array just just to have another way of doing it.

--- DnDice/ViewController.swift
+++ DnDice/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndexLeft: Int = 1
+    var randomDiceIndexRight: Int = 1
+    let diceArray = ["d1","d2","d3","d4","d5","d6"]
+    
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        randomDiceIndexLeft = Int.random(in:0 ... 5)
+        randomDiceIndexRight = Int.random(in:0 ... 5)
 
+//        No Array Block
+        diceImageViewLeft.image = UIImage(named: "d" +
+            "\(randomDiceIndexLeft)")
 
+        diceImageViewRight.image = UIImage(named: "d" + "\(randomDiceIndexRight)")
+
+        print(randomDiceIndexLeft, randomDiceIndexRight)
+   
+//        Array Block
+//        diceImageViewLeft.image = UIImage(named: diceArray[randomDiceIndexLeft])
+//
+//        diceImageViewRight.image = UIImage(named: diceArray[randomDiceIndexRight])
+    }
+    
 }
 
